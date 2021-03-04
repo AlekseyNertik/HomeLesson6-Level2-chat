f408946e09109b77047433a6ac6fabed799c34d9
AlekseyNertik <77322677+AlekseyNertik@users.noreply.github.com>
2021-03-05 00:03:38 +0700

    Update Client.java

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -9,41 +9,39 @@ public class Client {
         BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
         Scanner scanner = new Scanner(System.in);
 
-        String address = "localhost";
+        String address = "localhost"; 
         int serverPort = 8189;
 
         try {
             Socket socket;
-            InetAddress ipAddress = InetAddress.getByName(address);
+            InetAddress ipAddress = InetAddress.getByName(address); //цепляю адрес
             System.out.println("Подключение к  " + address + ":" + serverPort + "...");
             while (true) {
                 try {
-                    socket = new Socket(ipAddress, serverPort);
+                    socket = new Socket(ipAddress, serverPort); //цепляю адрес и порт на сокет
                     break;
                 } catch (ConnectException ignored) {
                 }
             }
-
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-            String line;
+            DataInputStream in = new DataInputStream(socket.getInputStream()); //поток сюда
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream()); //поток отсюда
+            String line; 
             System.out.println("Связь установлена!");
             System.out.println();
 
-            new Thread(() -> {
+            new Thread(() -> { //делаю отдельный поток на входящие сообщения
                 while (true) {
                     try {
-                        System.out.println(in.readUTF());
+                        System.out.println(in.readUTF()); //вывод входящих сообщений
                     } catch (IOException e) {
                     }
                 }
-            }).start();
+            }).start(); //собственно запуск потока
 
             while (true) {
-                line = keyboard.readLine();
-                out.writeUTF(line);
-                out.flush();
+                line = keyboard.readLine(); //в основном потоке читаю с клавы сообщение
+                out.writeUTF(line); //отправляю сообщение на сервер
+                out.flush(); 
             }
         } catch (Exception x) {
             x.printStackTrace();
