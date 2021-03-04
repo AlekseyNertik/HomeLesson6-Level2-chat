cd24867368ee680c70b3670036292f9392b3f972
AlekseyNertik <77322677+AlekseyNertik@users.noreply.github.com>
2021-03-05 00:12:02 +0700

    Update Server.java

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -7,32 +7,32 @@ public class Server {
     public static void main(String[] ar) {
         int port = 8189;
         try {
-            ServerSocket ss = new ServerSocket(port);
+            ServerSocket ss = new ServerSocket(port); //запускаю порт сервера
             System.out.println("Ожидание подключения...");
 
 
-            Socket socket = ss.accept();
+            Socket socket = ss.accept(); //цепляю входящего клиента на сокет
             System.out.println("Связь установлена!");
             System.out.println();
 
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            DataInputStream in = new DataInputStream(socket.getInputStream()); //поток сюда
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream()); //поток отсюда
 
-            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in)); //цепляю клавиатуру на поток
             String line;
 
-            new Thread(() -> {
+            new Thread(() -> { //отдельный поток на входящий поток
                 while (true) {
                     try {
-                        System.out.println(in.readUTF());
+                        System.out.println(in.readUTF()); //читаю и печатаю поток сюда
                     } catch (IOException e) {
                     }
                 }
-            }).start();
+            }).start();//собственно запуск потока
 
             while (true) {
-                line = keyboard.readLine();
-                out.writeUTF(line);
+                line = keyboard.readLine(); //считываю из буфера, что введено с клавы
+                out.writeUTF(line);//отправляю сообщение клиенту
                 out.flush();
             }
         } catch (Exception x) {
